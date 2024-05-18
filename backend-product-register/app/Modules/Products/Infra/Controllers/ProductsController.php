63ef6dfbd2b1f3aa5bb69b289e960b33fac73a35
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2024-05-18 01:32:36 -0300

    feat: configure redis for caching

--- backend-product-register/app/Modules/Products/Infra/Controllers/ProductsController.php
+++ backend-product-register/app/Modules/Products/Infra/Controllers/ProductsController.php
@@ -7,11 +7,13 @@ use App\Http\Responses\ApiResponse;
 use App\Modules\Products\UseCases\ListProductsUseCase;
 use App\Modules\Products\UseCases\SaveProductsUseCase;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class ProductsController extends Controller
 {
     private $saveProductsUseCase;
     private $listProductsUseCase;
+    const PRODUCTS_CACHE_KEY = 'products';
 
     public function __construct(SaveProductsUseCase $saveProductsUseCase, ListProductsUseCase $listProductsUseCase)
     {
@@ -22,7 +24,15 @@ class ProductsController extends Controller
     public function index(Request $request)
     {
         $perPage = $request->get('per_page') ?? null;
-        $products = $this->listProductsUseCase->execute($perPage);
+        $page = $request->get('page') ?? 1;
+        $cacheKey = ProductsController::PRODUCTS_CACHE_KEY . '_' . $page . '_' . $perPage;
+        $products = Cache::store('redis')->get($cacheKey);
+
+        if (!$products) {
+            $products = $this->listProductsUseCase->execute($perPage);
+            Cache::store('redis')->put($cacheKey, $products, now()->addHour());
+        }
+
         return ApiResponse::success($products);
     }
 
